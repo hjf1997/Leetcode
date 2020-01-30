3a045fe96fc7ab9650d18d6497187b0c1b417a75
JunfengHu <278053122@qq.com>
2020-01-30 11:31:30 +0800

    rectify divide-two-integers

--- Binary Search/divide-two-integers.py
+++ Binary Search/divide-two-integers.py
@@ -1,4 +1,5 @@
-
+# 36ms 78.42%
+# 13.2MB 56.69%
 class Solution:
 
     def __init__(self):
@@ -7,16 +8,12 @@ class Solution:
     def divide(self, dividend: int, divisor: int) -> int:
         if dividend > 0 and divisor < 0:
             ans = -self.binaryDivide(dividend, -divisor, 32, 0)
-
         elif dividend < 0 and divisor > 0:
             ans = -self.binaryDivide(-dividend, divisor, 32, 0)
-
         elif dividend < 0 and divisor < 0:
             ans = self.binaryDivide(-dividend, -divisor, 32, 0)
-
         else:
             ans = self.binaryDivide(dividend, divisor, 32, 0)
-
         if ans < -1 << 31 or ans > (1<<31) - 1:
             return (1 << 31) - 1
         else:
@@ -29,16 +26,13 @@ class Solution:
 
         upper = divisor << (left + right >> 1)
         if upper <= dividend:
-            if left == right:
+            if left - 1 == right:
                 self.ans += 1 << (left + right >> 1)
                 return self.binaryDivide(dividend-upper, divisor, left, 0)
             else:
-                if left - right == 1:
-                    return self.binaryDivide(dividend, divisor, left - 1, left + right >> 1)
-                else:
-                    return  self.binaryDivide(dividend, divisor, left, left + right >> 1)
+                return  self.binaryDivide(dividend, divisor, left, left + right >> 1)
         else:
             return self.binaryDivide(dividend, divisor, left + right >> 1, right)
 
 a = Solution()
-print(a.divide(-2147483648, -1))
+print(a.divide(10000, 3))

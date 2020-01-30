7367f58543a0a5d13061a69bf76bca0361b8a993
JunfengHu <278053122@qq.com>
2020-01-30 11:16:05 +0800

    rectify divide-two-integers

--- Binary Search/divide-two-integers.py
+++ Binary Search/divide-two-integers.py
@@ -1,6 +1,4 @@
 
-
-
 class Solution:
 
     def __init__(self):
@@ -8,32 +6,39 @@ class Solution:
 
     def divide(self, dividend: int, divisor: int) -> int:
         if dividend > 0 and divisor < 0:
-            divisor = -divisor
+            ans = -self.binaryDivide(dividend, -divisor, 32, 0)
+
         elif dividend < 0 and divisor > 0:
-            dividend = -dividend
+            ans = -self.binaryDivide(-dividend, divisor, 32, 0)
 
-        return self.binaryDivide(dividend, divisor, 32, 0)
+        elif dividend < 0 and divisor < 0:
+            ans = self.binaryDivide(-dividend, -divisor, 32, 0)
 
-    def judgeUpper(self, dividend, divisor, multiple: int):
-        if divisor << multiple >= dividend:
-            return False
-        return True
+        else:
+            ans = self.binaryDivide(dividend, divisor, 32, 0)
+
+        if ans < -1 << 31 or ans > (1<<31) - 1:
+            return (1 << 31) - 1
+        else:
+            return ans
     
     def binaryDivide(self, dividend, divisor, left: int, right: int):
+
         if divisor > dividend:
-            if self.ans > (1<< 31) - 1:
-                return (1<< 31) - 1
-            else:
-                return self.ans
-        upper = divisor << (left + right>> 1)
+            return self.ans
+
+        upper = divisor << (left + right >> 1)
         if upper <= dividend:
-            if self.judgeUpper(dividend, divisor, left + (left + right + 1>> 1) >> 1) is False:
-                self.ans += 1 << (left + right>> 1)
-                return self.binaryDivide(dividend-upper, divisor, (left + right>> 1), 0)
+            if left == right:
+                self.ans += 1 << (left + right >> 1)
+                return self.binaryDivide(dividend-upper, divisor, left, 0)
             else:
-                return self.binaryDivide(dividend, divisor, left, (left + right>> 1))
+                if left - right == 1:
+                    return self.binaryDivide(dividend, divisor, left - 1, left + right >> 1)
+                else:
+                    return  self.binaryDivide(dividend, divisor, left, left + right >> 1)
         else:
-            return self.binaryDivide(dividend, divisor, left + right>> 1, 0)
+            return self.binaryDivide(dividend, divisor, left + right >> 1, right)
 
 a = Solution()
-print(a.divide(1000, 1))
+print(a.divide(-2147483648, -1))

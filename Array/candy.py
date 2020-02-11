179f2c4573a200b00a904c28582b24e4f95db155
JunfengHu <278053122@qq.com>
2020-02-11 20:38:34 +0800

    solve the problems 135, 45

--- Array/candy.py
+++ Array/candy.py
@@ -1,31 +1,23 @@
 from typing import List
 import random
-
+# 184ms 89.93%
+# 15.4MB 72.44%
 class Solution:
 
     def candy(self, ratings: List[int]) -> int:
-        candy_list = [1]
-        for i in range(1, len(ratings)):
+        candy_list = [1 for i in range(len(ratings))]
 
+        for i in range(1, len(ratings)):
             if ratings[i] > ratings[i-1]:
-                candy_list.append(candy_list[-1]+1)
-            else:
-                candy_list.append(1)
-                for j in range(len(candy_list)-1, 0, -1):
-                    if ratings[j-1] > ratings[j] and candy_list[j-1] == candy_list[j]:
-                        candy_list[j - 1] += 1
-                    else:
-                        break
+                candy_list[i] += candy_list[i-1]
+
+        for i in range(len(ratings)-2, -1, -1):
+            if ratings[i] > ratings[i+1]:
+                candy_list[i] = max(candy_list[i+1] + 1, candy_list[i])
         return sum(candy_list)
 
-    def check(self, ratings, candys):
-        for i in range(1, len(ratings)):
-            if ratings[i] > ratings[i-1] and candys[i] <= candys[i-1]:
-                print('False')
-                return
-
 a = Solution()
-l = [4,4,3,3,3,2,2]
+l = [1,0,2]
 print(a.candy(l))
 # for i in range(500):
 #     rats = random.choices(range(1,100), k=50)
